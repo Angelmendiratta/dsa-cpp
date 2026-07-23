eef5cfb2b0a6b1d2e7cb76dd2edb662530474092
Angel Mendiratta <angelmendiratta6@gmail.com>
2026-07-23 13:38:22 +0530

    practicing patterns

---
+++ patterns/patterns.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+void pattern1(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                cout << "*";
+            }
+            cout << "\n";
+        }
+    }
+void pattern2(int n) {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                cout<<"*";
+            }
+            cout<<"\n";
+        }
+
+    }
+void pattern3(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                cout<<j;
+            }
+            cout<<endl;
+        }
+
+    }
+void pattern4(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                cout<<i;
+            }
+            cout<<endl;
+        }
+
+    }
+void pattern5(int n) {
+        for(int i=n ;i>=1; i--){
+            for(int j=1; j<=i; j++){
+                cout<<j;
+            }
+            cout<<endl;
+        }
+
+    }
+void pattern6(int n){
+    for(int i =1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+        cout<<j;
+         }
+    cout<<"\n";
+   }
+}
+void pattern7(int n){
+    for(int i=0;i<n;i++){
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+
+}
+void pattern8(int n){
+    
+}
+
+
+
+int main() {
+    int n = 5;
+
+    // Change only this line
+    pattern7(n);
+
+    return 0;
+}
